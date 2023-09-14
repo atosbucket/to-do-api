00b6ff83ad5e2954fb9be7f1a4c9dafad08f5268
develrocket <victory20201224@gmail.com>
2023-09-15 00:15:14 +0200

    init

---
+++ app/Http/Controllers/api/TaskController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Routing\Controller as BaseController;
+use App\Models\Todo;
+
+class TaskController extends BaseController {
+
+    public function index() {
+        $todos = Todo::all();
+        return response()->json($todos);
+    }
+
+    public function store(Request $request) {
+        $item = Todo::create($request->all());
+        return response()->json($item, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $item = Todo::findOrFail($id);
+        $item->update([
+            "name" => $request->get('name')
+        ]);
+        return resposne()->json($item);
+    }
+
+    public function destroy($id) {
+        $item = Todo::findOrFail($id);
+        $item->delete();
+        return response()->json(['message' => 'Item deleted!']);
+    }
+
+
+}
